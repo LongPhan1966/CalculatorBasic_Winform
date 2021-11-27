cbfede1ae3175f4d93d117cdf3263d82ed475e8a
Phan Hoàng Long <long.ph@3si.com.vn>
2021-11-27 16:13:09 +0700

    Tính toán cơ bản

---
+++ CalculatorBasic/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace CalculatorBasic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtketqua = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtketqua
+            // 
+            this.txtketqua.AutoSize = true;
+            this.txtketqua.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtketqua.Location = new System.Drawing.Point(380, 248);
+            this.txtketqua.Name = "txtketqua";
+            this.txtketqua.Size = new System.Drawing.Size(0, 29);
+            this.txtketqua.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(283, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập a";
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txta.Location = new System.Drawing.Point(74, 104);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(171, 28);
+            this.txta.TabIndex = 3;
+            // 
+            // txtb
+            // 
+            this.txtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb.Location = new System.Drawing.Point(288, 104);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(171, 28);
+            this.txtb.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(74, 205);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(114, 55);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Cộng";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(194, 205);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(114, 55);
+            this.btnSub.TabIndex = 6;
+            this.btnSub.Text = "Trừ";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(74, 277);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(114, 55);
+            this.btnMul.TabIndex = 7;
+            this.btnMul.Text = "Nhân";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(194, 277);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(114, 55);
+            this.btnDiv.TabIndex = 8;
+            this.btnDiv.Text = "Chia";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(380, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Kết quả";
+            // 
+            // txtkq
+            // 
+            this.txtkq.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkq.Location = new System.Drawing.Point(345, 237);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(171, 28);
+            this.txtkq.TabIndex = 10;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(372, 286);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(114, 36);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Đặt lại";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 375);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtketqua);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtketqua;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ CalculatorBasic/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorBasic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            float a, b, kq;
+
+            a = float.Parse(txta.Text);
+            b = float.Parse(txtb.Text);
+
+            kq = a + b;
+
+            txtkq.Text = kq.ToString();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            float a, b, kq;
+
+            a = float.Parse(txta.Text);
+            b = float.Parse(txtb.Text);
+
+            kq = a - b;
+
+            txtkq.Text = kq.ToString();
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            float a, b, kq;
+
+            a = float.Parse(txta.Text);
+            b = float.Parse(txtb.Text);
+
+            kq = a * b;
+
+            txtkq.Text = kq.ToString();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            float a, b, kq;
+
+            a = float.Parse(txta.Text);
+            b = float.Parse(txtb.Text);
+
+            kq = a / b;
+
+            txtkq.Text = kq.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txta.Text = "";
+            txtb.Text = "";
+            txtkq.Text = "";
+        }
+    }
+}
